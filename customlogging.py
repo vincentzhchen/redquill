00eda5ae8ac72c2b54d673c8271cc86121bd8226
Vincent Chen <>
2017-09-17 17:06:55 -0400

    removed decorator to get level statistics

--- customlogging.py
+++ customlogging.py
@@ -26,57 +26,55 @@ class CustomLogging(logging.Logger):
         self.error_count = 0
         self.warn_count = 0
 
-    def _level_counter(func):
-        @wraps(func)
-        def wrapper(self, *arg, **kwargs):
-            level = func(self, *arg, **kwargs)
-            if level == logging.ERROR:
-                self.error_count += 1
-            elif level == logging.WARNING:
-                self.warn_count += 1
-        return wrapper
+    def _collect_level_statistics(self, level):
+        if level == logging.ERROR:
+            self.error_count += 1
+        elif level == logging.WARNING:
+            self.warn_count += 1
 
     def _is_level_allowed(self, level):
         return self.level <= level
 
-    @_level_counter
+    def _is_dataframe(self, df):
+        if df.__class__ == pd.DataFrame:
+            return True
+        else:
+            self._log(logging.ERROR, "no dataframe was passed in...", None)
+            return False
+
     def error(self, msg, *args, **kwargs):
         if not self._is_level_allowed(level=logging.ERROR):
             return None
 
         self._log(logging.ERROR, msg, args, **kwargs)
-        return logging.ERROR
+        self._collect_level_statistics(level=logging.ERROR)
 
-    @_level_counter
     def warning(self, msg, *args, **kwargs):
         if not self._is_level_allowed(level=logging.WARNING):
             return None
 
         self._log(logging.WARNING, msg, args, **kwargs)
-        return logging.WARNING
+        self._collect_level_statistics(level=logging.WARNING)
 
-    @_level_counter
-    def log_exception(self, *args):
+    def log_exception(self):
         if not self._is_level_allowed(level=logging.ERROR):
             return None
 
-        self._log(logging.ERROR, "Stack...\n%s" % traceback.format_exc(), args)
-        return logging.ERROR
+        self._log(logging.ERROR, "Stack...\n%s" % traceback.format_exc(), None)
+        self._collect_level_statistics(level=logging.ERROR)
 
-    @_level_counter
-    def log_dataframe(self, df, msg="", level="INFO", *args):
+    def log_dataframe(self, df, msg="", level="INFO"):
         level = eval("logging." + level.upper())
         if not self._is_level_allowed(level=level):
             return None
 
-        if df.__class__ == pd.DataFrame:
-            self._log(level, msg + "\n" + df.to_string(), args)
-        else:
-            self._log(logging.ERROR, "no dataframe was passed in...", args)
-        return level
+        if not self._is_dataframe(df=df):
+            return None
 
-    @_level_counter
-    def dataframe_head_tail(self, df, n=5, h=None, t=None, msg="", level="INFO", *args):
+        self._log(level, msg + "\n" + df.to_string(), None)
+        self._collect_level_statistics(level=level)
+
+    def dataframe_head_tail(self, df, n=5, h=None, t=None, msg="", level="INFO"):
         """
         Logs the head and tail of a dataframe.
 
@@ -93,16 +91,27 @@ class CustomLogging(logging.Logger):
         if not self._is_level_allowed(level=level):
             return None
 
-        if df.__class__ == pd.DataFrame:
-            head = df.head(h) if h else df.head(n)
-            tail = df.tail(t) if t else df.tail(n)
-            self._log(level, msg + "\nHEAD\n" + head.to_string() + "\nTAIL\n" + tail.to_string(), args)
-        else:
-            self._log(logging.ERROR, "no dataframe was passed in...", args)
-        return level
+        if not self._is_dataframe(df=df):
+            return None
 
-    def get_error_count(self, *args):
-        self._log(logging.INFO, "TOTAL ERRORS: {}".format(self.error_count), args)
+        head = df.head(h) if h else df.head(n)
+        tail = df.tail(t) if t else df.tail(n)
+        self._log(level, msg + "\nHEAD\n" + head.to_string() + "\nTAIL\n" + tail.to_string(), None)
+        self._collect_level_statistics(level=level)
 
-    def get_warning_count(self, *args):
-        self._log(logging.INFO, "TOTAL WARNINGS: {}".format(self.warn_count), args)
+    def warn_duplicates(self, df, subset=None, msg="", keep=False):
+        if not self._is_level_allowed(level=logging.WARNING):
+            return None
+
+        if not self._is_dataframe(df=df):
+            return None
+
+        df = df[df.duplicated(subset=subset, keep=keep)]
+        self._log(logging.WARNING, msg + "\n" + df.to_string(), None)
+        self._collect_level_statistics(level=logging.WARNING)
+
+    def get_error_count(self):
+        self._log(logging.INFO, "TOTAL ERRORS: {}".format(self.error_count), None)
+
+    def get_warning_count(self):
+        self._log(logging.INFO, "TOTAL WARNINGS: {}".format(self.warn_count), None)
